2f30fb20c9f152b97983ddfbc151afdadbb40e6b
Helcya Franco <helcyafranco@gmail.com>
2020-11-26 03:46:42 -0300

    Exercícios da Aula 04

---
+++ Helcya_Franco_Q3.py
@@ -0,0 +1,11 @@
+partidas = int(input('Quantos jogos você deseja?'))
+mega_sena = []
+jogo = []
+mega_sena.append(jogo[:])
+from random import sample
+def jogo():
+    return sorted(sample(range(1, 60), 6))
+i = 0
+for i in range(partidas):
+    print('Jogo',i+1,':',jogo())
+    i += 1 
